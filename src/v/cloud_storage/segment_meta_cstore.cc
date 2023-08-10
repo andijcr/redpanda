aa623fe18835cf70d27e99ec78edeec6de691286
Andrea Barbadoro <andrea@redpanda.com>
2023-08-10 15:40:43 +0200

    experiments

--- src/v/cloud_storage/segment_meta_cstore.cc
+++ src/v/cloud_storage/segment_meta_cstore.cc
@@ -268,7 +268,7 @@ public:
     column_store() = default;
 
     /// Add element to the store. The operation is transactional.
-    void append(const segment_meta& meta) {
+    void append(const segment_meta& meta, bool append_hint=true) {
         auto ix = _base_offset.size();
 
         try {
@@ -285,9 +285,9 @@ public:
             vassert(false, "column_store bad_alloc during 'append' operation");
         }
 
-        if (
-          ix
-            % static_cast<uint32_t>(::details::FOR_buffer_depth * sampling_rate)
+        if (append_hint &&
+          (ix
+            % static_cast<uint32_t>(::details::FOR_buffer_depth * sampling_rate))
           == 0) {
             // At the beginning of every row we need to collect
             // a set of hints to speed up the subsequent random
@@ -365,6 +365,7 @@ public:
         // extract the end iterator for hints, to cover only the frames that
         // will be cloned
         auto last_hint_tosave = [&] {
+          return _hints.end();
             auto frame_it = _base_offset.get_frame_iterator_by_element_index(
               first_replacement_index);
             if (frame_it == _base_offset._frames.begin()) {
@@ -416,7 +417,7 @@ public:
             // append old segments with committed offset smaller than
             // replacement
             while (old_seg.committed_offset < replacement_base_offset) {
-                replacement_store.append(old_seg);
+                replacement_store.append(old_seg, false);
                 details::increment_all(old_segments_it);
                 if (old_segments_it == old_segments_end) {
                     break;
@@ -426,7 +427,7 @@ public:
             // old_segment_it points to first segment to replace
 
             // append replacement segment instead of the old one
-            replacement_store.append(replacement_meta);
+            replacement_store.append(replacement_meta, false);
 
             // skip over segments superseded by replacement_meta
             while (old_segments_it != old_segments_end) {
@@ -539,8 +540,13 @@ public:
     auto at_with_hint(
       const col_t& c, uint32_t ix, const std::optional<hint_vec_t>& h) const {
         vassert(h.has_value(), "Invalid access at index {}", ix);
+        try{
         return c.at_index(
           ix, std::get<static_cast<size_t>(col_index)>(h.value()));
+        }catch(std::out_of_range& e){
+          vlog(cst_log.error, "{} index {} Out of range error: {}", __PRETTY_FUNCTION__, ix, e.what());
+          throw;
+        }
     }
 
     /// Materialize 'segment_meta' struct from column iterator
